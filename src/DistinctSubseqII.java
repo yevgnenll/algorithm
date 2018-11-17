7e67b44fa54d3cbefeaa7fa6fa32822f42784405
seungkwon <yevgnenll@gmail.com>
2018-11-17 23:08:14 +0900

    [#10] TDD를 이용해 알고리즘을 해결한다.

---
+++ src/DistinctSubseqII.java
@@ -0,0 +1,28 @@
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DistinctSubseqII {
+
+  public int distinctSubseqII(String S) {
+
+    return 0;
+  }
+
+  @Test
+  public void abc를_입력하면_7이_결과로_나온다() {
+
+  }
+
+  @Test
+  public void aba를_입력하면_결과는_6이다() {
+
+  }
+
+  @Test
+  public void aaa를_입력하면_결과는_3이다() {
+
+  }
+
+}
