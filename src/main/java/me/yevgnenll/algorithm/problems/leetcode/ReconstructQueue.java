1d9daadedb855779705a2595901bb84a4fc063ff
박승권 <yevgnenll@gmail.com>
2021-02-02 02:31:19 +0900

    effective java 14번 적용

--- src/main/java/me/yevgnenll/algorithm/problems/leetcode/ReconstructQueue.java
+++ src/main/java/me/yevgnenll/algorithm/problems/leetcode/ReconstructQueue.java
@@ -2,18 +2,19 @@ package me.yevgnenll.algorithm.problems.leetcode;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Objects;
 
 public class ReconstructQueue {
 
   public int[][] reconstructQueue(int[][] people) {
-    Arrays.sort(people, (o1, o2) ->
-        o1[0] != o2[0] ? - o1[0] + o2[0] : o1[1] - o2[1]
+    Arrays.sort(people, (o1, o2) -> !Objects.equals(o1[0], o2[0]) ?
+        Integer.compare(o2[0], o1[0]) : Integer.compare(o1[1], o2[1])
     );
 
     LinkedList<int[]> list = new LinkedList<>();
     for (int i = 0; i < people.length; i++)
       list.add(people[i][1], people[i]);
-    return people;
+    return list.toArray(new int[people.length][2]);
   }
 
 }
